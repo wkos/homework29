c220dc9869aa161e9e22de3f763665c28b74d2dd
Wojciech Kos <obohp@wp.pl>
2020-09-26 08:38:49 +0200

    Initial commit

---
+++ src/main/java/pl/wkos/homework29/Homework29Application.java
@@ -0,0 +1,13 @@
+package pl.wkos.homework29;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Homework29Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Homework29Application.class, args);
+    }
+
+}
---
+++ src/test/java/pl/wkos/homework29/Homework29ApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.wkos.homework29;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Homework29ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
